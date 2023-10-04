80f2978eeff084f51f43c230f8aa5ab76ad089c0
SinYoonHa <idc06012@naver.com>
2023-10-04 10:12:36 +0900

    Backend #15 클라이언트 사이드 렌더링, JWT Token, Spring Security를 이용한 로그인 로직 구현

--- src/main/java/com/example/todo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/todo/security/JwtAuthenticationFilter.java
@@ -30,11 +30,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse
 			response, FilterChain filterChain) throws ServletException, IOException {
 	try {
+		//request에서 토큰 가져오기
 		String token =parseBearerToken(request);
 		log.info("Filter is running...");
+		
+		//토큰 검사하기. JWT이므로 인가 서버에 요청하지않고도 검증 가능.
 		if(token !=null &&!token.equalsIgnoreCase("null")) {
-			String userId = tokenProvider.validateAndGetUserId(token);
+			String userId = tokenProvider.validateAndGetUserId(token); //userId 가져오기. 위조 된 경우 예외처리
 			log.info("Authenticated user ID : "+ userId);
+			
+			//인증완료: SecurityContextHolder에 등록해야 인증된 사용자의 정보. 문자열이 아니어도 아무거나 입력가능.
 			AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
 					userId,null, AuthorityUtils.NO_AUTHORITIES);
 			authentication.setDetails ( new WebAuthenticationDetailsSource().buildDetails(request));
@@ -45,14 +50,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 	}catch(Exception ex){
 		logger.error("Could not set user authentication in security context",ex);
 	}
+	
 	filterChain.doFilter(request, response);
 	}
-	private String parseBearerToken(HttpServletRequest request){
-	String bearerToken = request.getHeader("Authorization");
 	
-	if (StringUtils.hasText ( bearerToken) && bearerToken.startsWith("Bearer")){
-		return bearerToken.substring(7);
-	}
+	private String parseBearerToken(HttpServletRequest request){
+		//Http Request의 헤더를 파싱해 Bearer 토큰을 리턴한다.
+		String bearerToken = request.getHeader("Authorization");
+	
+		if (StringUtils.hasText ( bearerToken) && bearerToken.startsWith("Bearer")){
+			return bearerToken.substring(7);
+		}
 		return null;
 	}
 }
